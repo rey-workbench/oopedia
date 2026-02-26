9464090e5cb5be006d098689b3c41dcd6fd2bac6
reysilvaa <reynaldsilva123@gmail.com>
2026-02-26 14:15:20 +0700

    redeploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Material;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         View::composer('components.navbars.sidebar', function ($view) {
             $view->with('materials', Material::orderBy('created_at', 'asc')->get());
         });
