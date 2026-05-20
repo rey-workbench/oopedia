bc47672d74635d27eb8f79affc6161fbf96809de
reysilvaa <reynaldsilva123@gmail.com>
2026-05-20 23:52:43 +0700

    chore: remove project documentation and testing legacy files

--- test_order.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$q = \App\Models\Question::where('material_id', '01kqt8tymds2swgcc7r2bcq5nv')->orderBy('id')->get();
-$s = $q->groupBy('difficulty')->map(function($g) { return $g->shuffle(); })->flatten(1)->pluck('difficulty')->toJson();
-echo $s;
